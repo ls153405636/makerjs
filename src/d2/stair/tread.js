87731dd2d4cefed21b67fe5fda7c9d61dfc6808f
ls <153405636@qq.com>
2021-10-15 16:04:59 +0800

    16点04分

--- src/d2/stair/tread.js
+++ src/d2/stair/tread.js
@@ -1,17 +1,16 @@
-import { Types } from "../../types/stair_v2";
-import { BaseWidget } from "../base_widget";
-
+import { Types } from '../../types/stair_v2'
+import { BaseWidget } from '../base_widget'
 
 export class Tread extends BaseWidget {
   /**
-   * 
-   * @param {Types.Tread} vPB 
+   *
+   * @param {Types.Tread} vPB
    */
-  constructor (vPB, vParent) {
+  constructor(vPB, vParent) {
+    super()
     this.outline = vPB.outline
     this.parent = vParent
   }
 
-  draw () {
-  }
+  draw() {}
 }
