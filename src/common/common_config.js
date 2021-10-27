4dc51cce5c1df5a337e4d0aa1cbd2058f815b513
pengpeng <hanann18@163.com>
2021-10-27 18:36:41 +0800

    解决冲突

--- src/common/common_config.js
+++ src/common/common_config.js
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 
 export const COMP_TYPES = {
-=======
-const COMP_TYPES = {
->>>>>>> bb166d5c54b88c6c910bf0bf0f94714d27885cce
   HOLE: 1,
   WALL: 2,
   STAIR: 3,
@@ -14,13 +10,8 @@ const COMP_TYPES = {
   TREAD: 8,
 }
 
-<<<<<<< HEAD
 export const CUR_DATA = {
   SELECTED_TYPE: COMP_TYPES.HOLE,
-=======
-const CUR_DATA = {
-  // SELECTED_TYPE: COMP_TYPE.HOLE,
->>>>>>> bb166d5c54b88c6c910bf0bf0f94714d27885cce
   SELECTED_ID: '',
   MODE: '2D',
 }
