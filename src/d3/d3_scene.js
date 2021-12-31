2caa36e3a2753a46600060b66bd7ce19af36c77d
pengpeng <hanann18@163.com>
2021-12-31 18:14:31 +0800

    删除网格和坐标轴

--- src/d3/d3_scene.js
+++ src/d3/d3_scene.js
@@ -25,11 +25,11 @@ export class D3Scene {
 
     this.control = new OrbitControls(this.camera, this.renderer.domElement)
 
-    this.grider = new THREE.GridHelper(20000, 20, 0xc1c2c3, 0xc1c2c3)
-    this.scene.add(this.grider)
+    // this.grider = new THREE.GridHelper(20000, 20, 0xc1c2c3, 0xc1c2c3)
+    // this.scene.add(this.grider)
 
-    this.axes = new THREE.AxesHelper(20000)
-    this.scene.add(this.axes)
+    // this.axes = new THREE.AxesHelper(20000)
+    // this.scene.add(this.axes)
 
     this.initLight()
   }
